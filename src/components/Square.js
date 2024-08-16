27b0c37d909df028fdad808f52b2bd97e9245d46
naveen <naranaveen576@gmail.com>
2024-08-16 11:09:14 +0530

    intial commit

---
+++ src/components/Square.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Square() {
+  return (
+    <>
+    <div className="square">
+      <button className="square">1</button>
+     
+    </div>
+  
+  </>  )
+}
