ecd24b479d5a73c9f083d23b9a29de0d2db87b28
naveen <naranaveen576@gmail.com>
2024-08-16 12:25:14 +0530

    added winner to the board

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,16 +2,39 @@ import React, { useState } from "react";
 import Square from "./Square";
 
 function Board() {
+ 
+
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+  
 
   function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
     <>
+      <div className="status">{status}</div>
+
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -31,6 +54,26 @@ function Board() {
   );
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 export default Board;
 
 //function handleclick is getting invoked when the square[button] is getting rendered
@@ -38,3 +81,6 @@ export default Board;
 
 //function handleClick will be invoked only when the user clicks the button
 //<Square value={squares[3]} onSquareClick={() => handleClick(0)} />
+
+//when to use callback... ur not sure  how much time the function will take to complete
+//network call
